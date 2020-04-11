8e6e1f3f871a64437c8a8f48d289c4fa9e910596
GozaRuu <kais.sghari@gmail.com>
2020-04-11 14:08:08 +0200

    add es settings to Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
+
 class Post < ApplicationRecord
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
+  settings do
+    mappings dynamic: false do
+      indexes :author, type: :text
+      indexes :title, type: :text, analyzer: :english
+      indexes :body, type: :text, analyzer: :english
+      indexes :tags, type: :text, analyzer: :english
+      indexes :published, type: :boolean
+    end
+  end
 end
